ed94d618ceecf78e0f0788207b952ece94690771
Georgy <georgyrus@mail.ru>
2022-04-11 00:32:21 +0300

    add test_rational.py

---
+++ test_rational.py
@@ -0,0 +1,11 @@
+import rational as r
+
+m1 = 1
+n1 = 2
+m2 = 3
+n2 = 4
+
+print(r.sum (m1, n1, m2, n2))
+print(r.sub (m1, n1, m2, n2))
+print(r.mult (m1, n1, m2, n2))
+print(r.div (m1, n1, m2, n2))
